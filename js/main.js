5a1e514264c4ecfdcfdfd44bcf54928c838d01d9
Zachary Watkins <watkinza@gmail.com>
2016-10-11 16:57:32 -0500

    Refactor approach to be object-oriented, and allow multiple instances per page

--- js/main.js
+++ js/main.js
@@ -1,85 +1,130 @@
-var listfilter = {
-  form: document.querySelector('form.listitem-filterbox'),
-  init: null,
-  ele: null,
-  items: {
-    eles: [],
-    text: [],
-  },
-  handler: null,
-  usetitles: null,
-  preventSubmit: function(e){
+function Listfilter(form){
+  this.form = form;
+  this.ele = this.form.querySelector('input');
+  this.usetitles = (this.form.dataset.lifbUseTitles === 'true');
+
+  var items = {
+    eles: form.parentNode.getElementsByTagName('li'),
+    text: []
+  };
+
+  this.init = function(){
+
+    // Collect the list items
+    if(items.eles.length > 0){
+
+      // Pass search text from elements to a stored array
+      for(var i = 0; i < items.eles.length; i++){
+
+        var html = items.eles[i].innerHTML,
+            main = '',
+            title = '',
+            all = [];
+
+        // Isolate main element text
+        main = html.replace(/<[^<>]+>/g,'');
+
+        // Isolate title attribute text
+        if(this.usetitles){
+
+          // Isolate first title in html, replace commas with spaces, remove extra spaces
+          title = html.match(/title="([^"]*)"/);
+          if(title)
+            title = title[1].replace(/\s*,\s*/g,' ').replace(/\s+/,' ');
+
+        }
+
+        // Combine text to be stored
+        if(main != '')
+          all.push(main);
+        if(title != '')
+          all.push(title);
+
+        // Store text
+        items.text.push(all.join(' '));
+
+      }
+
+      // Initialize the search box
+      this.ele.addEventListener('keyup', this.handler);
+
+      // Prevent the form from being submitted
+      this.form.addEventListener('submit', this.preventSubmit);
+
+    }
+  };
+
+  this.handler = function(e){
+
+    var value = e.target.value,
+        indexes = [],
+        valuesToCheck = [];
+
+    // Remove the commas and spaces at the ends of the search string
+    value = value.replace(/^\s+/, '').replace(/\s+$/, '').replace(/,/g, '');
+
+    if(value != ''){
+
+      // If the search box has multiple words, search for them individually in addition to the whole value
+      if(value.indexOf(' ') > 0){
+        valuesToCheck = value.split(' ');
+      }
+
+      // Add the full value to the checked group
+      valuesToCheck.push(value);
+
+      // Loop through each stored item
+      for(var i = 0; i < items.text.length; i++){
+
+        var itemtext = items.text[i].toLowerCase(),
+            found = false;
+
+        // Loop through each word in the search string
+        for(var j = 0; j < valuesToCheck.length; j++){
+
+          if(itemtext.indexOf(valuesToCheck[j]) >= 0){
+
+            indexes.push(i);
+            found = true;
+            break;
+
+          }
+
+        }
+
+        // Hide unmatched stored items
+        if(!found) {
+          items.eles[i].style.display = 'none';
+        }
+
+      }
+
+      // Reset stored items matched with search string
+      for(var i = 0; i < indexes.length; i++){
+        items.eles[indexes[i]].style.display = '';
+      }
+
+    }
+
+  };
+
+  this.preventSubmit = function(e){
+
     e.preventDefault();
     return false;
-  }
+
+  };
+
+  this.init();
 }
-listfilter.init = function(){
-  // Collect the list items
-  listfilter.items.eles = listfilter.form.parentNode.getElementsByTagName('li');
-  if(listfilter.items.eles.length > 0){
-    // Pull data attributes from form element
-    listfilter.usetitles = (listfilter.form.dataset.lifbUseTitles === 'true');
-    // Pass search text from elements to a stored array
-    for(var i = 0; i < listfilter.items.eles.length; i++){
-      var html = listfilter.items.eles[i].innerHTML,
-          main = '',
-          title = '',
-          all = [];
-      // Isolate main element text
-      main = html.replace(/<[^<>]+>/g,'');
-      // Isolate title attribute text
-      if(listfilter.usetitles){
-        // Isolate first title in html, replace commas with spaces, remove extra spaces
-        title = html.match(/title="([^"]*)"/);
-        if(title)
-          title = title[1].replace(/\s*,\s*/g,' ').replace(/\s+/,' ');
-      }
-      // Combine text to be stored
-      if(main != '')
-        all.push(main);
-      if(title != '')
-        all.push(title);
-      // Store text
-      listfilter.items.text.push(all.join(' '));
-    }
-    // Initialize the search box
-    listfilter.ele = listfilter.form.querySelector('input');
-    listfilter.ele.addEventListener('keyup', listfilter.handler);
-    // Prevent the form from being submitted
-    listfilter.form.addEventListener('submit', listfilter.preventSubmit);
+
+// For each instance of shortcode, create new list filter object
+window.addEventListener('load', function(){
+
+  var forms = document.querySelectorAll('form.listitem-filterbox');
+
+  for(var i = 0; i < forms.length; i++){
+    new Listfilter(forms[i]);
   }
-};
-listfilter.handler = function(e){
-  var value = listfilter.ele.value,
-      indexes = [],
-      valuesToCheck = [];
-  // Remove the commas and spaces at the ends of the search string
-  value = value.replace(/^\s+/, '').replace(/\s+$/, '').replace(/,/g, '');
-  if(value != ''){
-    // If the search box has multiple words, search for them individually in addition to the whole value
-    if(value.indexOf(' ') > 0){
-      valuesToCheck = value.split(' ');
-    }
-    // Add the full value to the checked group
-    valuesToCheck.push(value);
-    // Loop through each item
-    for(var i = 0; i < listfilter.items.text.length; i++){
-      var itemtext = listfilter.items.text[i].toLowerCase(),
-          found = false;
-      for(var j = 0; j < valuesToCheck.length; j++){
-        if(itemtext.indexOf(valuesToCheck[j]) >= 0){
-          indexes.push(i);
-          found = true;
-          break;
-        }
-      }
-      if(!found) {
-        listfilter.items.eles[i].style.display = 'none';
-      }
-    }
-    for(var i = 0; i < indexes.length; i++){
-      listfilter.items.eles[indexes[i]].style.display = '';
-    }
-  }
-};
-window.addEventListener('load', listfilter.init);
+
+});
